1a794d21c48ff05c1d75259a5aabdb61227cdb82
ConnorCurry <connor4424@gmail.com>
2021-03-27 14:04:30 -0400

    updated getFirstAnimal

--- src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
+++ src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
@@ -2,5 +2,13 @@ package edu.ithaca.dragon.wildlife;
 
 public class Trainer {
     private Animal[] animals = new Animal[6];
-    
+
+    public Trainer() {
+
+    }
+
+    // returns first animal in party
+    public Animal getFirstAnimal() {
+        return null;
+    }
 }
--- src/main/java/edu/ithaca/dragon/wildlife/WildBattle.java
+++ src/main/java/edu/ithaca/dragon/wildlife/WildBattle.java
@@ -5,4 +5,9 @@ public class WildBattle extends Battle {
     public WildBattle(Player player, Trainer trainer, Climate climate) {
         super(player, trainer, climate);
     }
+
+    //ends battle
+    public void run() {
+        
+    }
 }
