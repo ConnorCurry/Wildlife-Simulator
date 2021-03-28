d35d473c592963f6c48511e0aaf459b20ed8034f
Pax <pmeny@ithaca.edu>
2021-03-28 13:40:12 -0400

    + Animal vars maxHP, currentHP, ad; +constructor

--- src/main/java/edu/ithaca/dragon/wildlife/Animal.java
+++ src/main/java/edu/ithaca/dragon/wildlife/Animal.java
@@ -2,4 +2,16 @@ package edu.ithaca.dragon.wildlife;
 
 public class Animal {
     private Move[] moves = new Move[4];
+    private int maxHP; // HP = Health Points
+    private int currentHP;
+    private int ad; //ad = Attack Damage
+
+    //Constructor
+    public Animal(int hp, int ad) {
+        this.maxHP = hp;
+        this.currentHP = hp;
+        this.ad = ad;
+    }
+
+
 }
