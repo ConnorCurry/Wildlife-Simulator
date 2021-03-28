13797b3c700df909e88ee240d887090537c9d757
Pax <pmeny@ithaca.edu>
2021-03-28 15:47:16 -0400

    +Trainer.getNextAnimal() gets first valid animal

--- src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
+++ src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
@@ -10,7 +10,21 @@ public class Trainer {
     // returns first animal in party
     public Animal getFirstAnimal() {
         
-        return this.animals[0];
+        return(this.animals[0]);
+    }
+
+    //returns first valid animal in party - if none returns null
+    public Animal getNextAnimal() {
+        Animal a;
+        for(int i = 0; i < this.animals.length; i++) {
+            if(this.animals[i] != null) {
+                if(this.animals[i].getCurrentHP() > 0) {
+                    return(this.animals[i]);
+                }
+                
+            }
+        }
+        return(null);
     }
 
     public void setAnimals(Animal [] as) {
