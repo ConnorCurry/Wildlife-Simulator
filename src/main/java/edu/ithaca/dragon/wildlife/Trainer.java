320fa6f2a18be4abb847c23645afbac143c29364
Pax <pmeny@ithaca.edu>
2021-03-28 14:23:54 -0400

    +Animals.setAnimals() for testing purposes

--- src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
+++ src/main/java/edu/ithaca/dragon/wildlife/Trainer.java
@@ -3,12 +3,17 @@ package edu.ithaca.dragon.wildlife;
 public class Trainer {
     private Animal[] animals = new Animal[6];
 
-    public Trainer() {
+   /*  public Trainer() {
 
-    }
+    } */
 
     // returns first animal in party
     public Animal getFirstAnimal() {
-        return null;
+        
+        return this.animals[0];
+    }
+
+    public void setAnimals(Animal [] as) {
+        this.animals = as;
     }
 }
