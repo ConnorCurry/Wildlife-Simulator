f8d5346dea6c0c2466ef7abc8dee5a2efed658ba
Pax <pmeny@ithaca.edu>
2021-03-28 14:23:25 -0400

    +Trainer.setAnimals() for testing purposes

---
+++ src/test/java/edu/ithaca/dragon/BattleTest.java
@@ -0,0 +1,15 @@
+package edu.ithaca.dragon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BattleTest {
+
+    @Test
+    void opponentAttackGetBestMoveTest() {
+        Player p = new Player();
+        Battle b = new Battle();
+    }
+    
+}
