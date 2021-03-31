0cc07b745e4e7a2b436e92acde4c47b337be1156
ConnorCurry <connor4424@gmail.com>
2021-03-31 19:58:59 -0400

    Returned player that wins from startBattle

--- src/main/java/edu/ithaca/dragon/wildlife/WildlifeSimulator.java
+++ src/main/java/edu/ithaca/dragon/wildlife/WildlifeSimulator.java
@@ -45,7 +45,7 @@ public class WildlifeSimulator {
         
         } while (currBattle.getWinner() == null);
         scan.close();
-        return null;
+        return currBattle.getWinner();
     }
     
     /*
