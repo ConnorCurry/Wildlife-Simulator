18e81b2f8f8c7c6ffc9b3e1b5ac9718bc82dee76
ConnorCurry <connor4424@gmail.com>
2021-03-31 19:30:43 -0400

    getMoveFromTitleTest

--- src/test/java/edu/ithaca/dragon/wildlife/WildlifeSimulatorTest.java
+++ src/test/java/edu/ithaca/dragon/wildlife/WildlifeSimulatorTest.java
@@ -1,11 +1,34 @@
 package edu.ithaca.dragon.wildlife;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class WildlifeSimulatorTest {
     
     @Test
     public void getMoveFromTitleTest() {
-        
+        Animal animal1 = new Animal(1, 1, 100);
+        Move move1 = new Move("scratch", 1, 10);
+        Move[] moves = {move1, null, null, null};
+        animal1.setMoves(moves);
+        assertEquals(move1, WildlifeSimulator.getMoveFromTitle(animal1, "scratch")); // base case
+
+
+        Move move2 = new Move("tackle", 1, 10);
+        Move move3 = new Move("bite", 1, 10);
+        Move move4 = new Move("slash", 1, 10);
+        moves[1] = move2;
+        moves[2] = move3;
+        moves[3] = move4;
+        animal1.setMoves(moves);
+        assertEquals(move1, WildlifeSimulator.getMoveFromTitle(animal1, "scratch"));
+        assertEquals(move2, WildlifeSimulator.getMoveFromTitle(animal1, "tackle"));
+        assertEquals(move3, WildlifeSimulator.getMoveFromTitle(animal1, "bite"));
+        assertEquals(move4, WildlifeSimulator.getMoveFromTitle(animal1, "slash")); // full moveset test
+
+        moves[3] = null;
+        assertThrows(NullPointerException.class, () -> {WildlifeSimulator.getMoveFromTitle(animal1, "slash");}); // move doesnt exist
     }
 }
