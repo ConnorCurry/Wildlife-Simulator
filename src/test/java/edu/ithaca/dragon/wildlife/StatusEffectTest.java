bfc78e83c7c61bbb89bd8ca5ece4f08de16b1870
Pax <pmeny@ithaca.edu>
2021-04-15 11:37:42 -0400

    +getStatusEffectTest()

--- src/test/java/edu/ithaca/dragon/wildlife/StatusEffectTest.java
+++ src/test/java/edu/ithaca/dragon/wildlife/StatusEffectTest.java
@@ -8,10 +8,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatusEffectTest {
 
     @Test
-    void GetStatusEffectTest(){
+    void getStatusEffectTest(){
         GetStatusEffect gse = new GetStatusEffect();
+        //Test Poison
         StatusEffect p = gse.getPoison();
-        assertEquals(6, actual);
+        assertNotNull(p);
+        assertEquals(6, p.getLife());
+        assertEquals(5, p.getDamage());
+        //Test Bleed
+        StatusEffect bl = gse.getBleed();
+        assertNotNull(bl);
+        assertEquals(9, bl.getLife());
+        assertEquals(3, bl.getDamage());
+        //Test Burn
+        StatusEffect b = gse.getBurn();
+        assertNotNull(b);
+        assertEquals(4, b.getLife());
+        assertEquals(7, b.getDamage());
+    }
+
+    @Test
+    void moveWithStatusEffectTest() {
+        
     }
     
 }
