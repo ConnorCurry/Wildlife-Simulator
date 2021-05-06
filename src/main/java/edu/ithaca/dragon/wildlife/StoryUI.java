765239918dbe58c1d36ac8240a13851d9dcf8778
Ross Doane <rdoane@ithaca.edu>
2021-05-05 23:35:44 -0400

    Finished merge of pull request

--- src/main/java/edu/ithaca/dragon/wildlife/StoryUI.java
+++ src/main/java/edu/ithaca/dragon/wildlife/StoryUI.java
@@ -16,8 +16,8 @@ public class StoryUI {
         //Select starter animal
         int ans = -1;
         while(ans <= 0){ //Will run as long as ans != 1, 2, or 3
-            System.out.println("As you are a fresh spawn, we are blessing you with your choice of three different animals to start with.")
-            System.out.println("Input the number relative to your choice of the three.")
+            System.out.println("As you are a fresh spawn, we are blessing you with your choice of three different animals to start with.");
+            System.out.println("Input the number relative to your choice of the three.");
             System.out.println("1). Cat");
             System.out.println("2). Snake");
             System.out.println("3). Hawk");
@@ -28,13 +28,13 @@ public class StoryUI {
                 neo.addToParty(starterAnimals.get("cat"));
             } else if(ans == 2) {
                 neo.addToParty(starterAnimals.get("snake"));
-            } else if ans == 3) {
+            } else if (ans == 3) {
                 neo.addToParty(starterAnimals.get("hawk"));
             } else {
                 System.out.println("Invalid input. Please try again, this time either inputting 1, 2, or 3");
             }
         }
-        System.out.println("That's all you'll be needing in the Wilderness, now get out.")
+        System.out.println("That's all you'll be needing in the Wilderness, now get out.");
         
         //Quest Begins, split into 4 parts (Plains , Tundra , Desert (5 trainers, 4 wild animals), Swamp (6 trainers, 5 wild animals))
             //Plains-Area 1 ; (1 trainer, 2 wild animals)
@@ -50,7 +50,7 @@ public class StoryUI {
         }
         //second animal battle 2/2w
         System.out.println("As you leave the viscinity of your prior opponent, you are hit in the head out of the blue by an Animal camoflauged by its surroundings");
-        System.out.println("You have successfully passed the Plains Area")
+        System.out.println("You have successfully passed the Plains Area");
             //Tundra-Area 2; (2 trainers, 2 wild animals)
         //1/2 w
         System.out.println("All of a sudden, from out of the dirt, springs a wild animal!");
@@ -59,7 +59,7 @@ public class StoryUI {
         //2/2 w
         System.out.println("You are running from a stampede when you suddenly stop hearing footsteps. As you look behind you to see no animals chasing you, you hear a deep roar from your front side");
         //2/2 t
-        System.out.println("You think you are clear of the tundra when a Trainer named The Gatekeeper steps in your way ")
+        System.out.println("You think you are clear of the tundra when a Trainer named The Gatekeeper steps in your way ");
             //Desert-Area 3; (3 trainers, 2 wild animals)
         //1/3 t
         //1/2 w
