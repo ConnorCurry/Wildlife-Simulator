624fb9f06c3101ed328473f13e1b8fa3b0ab2cd0
ConnorCurry <connor4424@gmail.com>
2021-05-05 23:15:06 -0400

    Syntax cleanup

--- src/main/java/edu/ithaca/dragon/wildlife/Move.java
+++ src/main/java/edu/ithaca/dragon/wildlife/Move.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Move {
